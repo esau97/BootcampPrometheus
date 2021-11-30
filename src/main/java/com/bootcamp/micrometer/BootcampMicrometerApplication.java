f22111acd4e0419637918bf930f92d026a0a17eb
Esau <esauhp97@gmail.com>
2021-11-30 14:59:38 +0100

    First commit

---
+++ src/main/java/com/bootcamp/micrometer/BootcampMicrometerApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.micrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampMicrometerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/micrometer/BootcampMicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampMicrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
